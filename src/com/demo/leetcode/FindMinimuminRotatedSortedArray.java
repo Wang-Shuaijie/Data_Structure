d1438db1a98fc45f60632aeaf715a3babbd8a9e1
WangShuaijie <2283613941@qq.com>
2019-05-21 11:15:08 +0800

    auto commit

--- src/com/demo/leetcode/FindMinimuminRotatedSortedArray.java
+++ src/com/demo/leetcode/FindMinimuminRotatedSortedArray.java
@@ -6,26 +6,24 @@ public class FindMinimuminRotatedSortedArray {
 	 * 输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。 
 	 * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 
 	 * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0
+	 * 
+	 * 首先要判断这个有序数组是否旋转了，通过比较第一个和最后一个数的大小，如果第一个数小，则没有旋转，直接返回这个数。
+	 * 如果第一个数大，就要进一步搜索。我们定义left和right两个指针分别指向开头和结尾，
+	 * 还要找到中间那个数，然后和left指的数比较，如果中间的数大，则继续二分查找右半段数组，
+	 * 反之查找左半段。终止条件是当左右两个指针相邻，返回小的那个。
 	 */
-	public int findMin(int[] num) {
-		int low = 0, high = num.length - 1;
-		int ret = Integer.MAX_VALUE;
-
-		while (low <= high) {
-			int mid = low + (high - low) / 2;
-			// On the sorted part.
-			if (num[low] <= num[high]) {
-				return Math.min(num[low], ret);
-			} else { // On the rotated part.
-				// num[mid] is on the left rotated part.
-				if (num[mid] >= num[low]) {
-					low = mid + 1;
-				} else { // num[mid] is on the right rotated part.
-					ret = num[mid];
-					high = mid - 1;
-				}
+	public int findMin(int[] nums) {
+		if(nums.length==1) return nums[0];
+		int left = 0, right = nums.length - 1;
+		if(nums[left]<nums[right]) return nums[0];
+		while(left+1 != right) {
+			int mid=left+(right-left)/2;
+			if(nums[mid]<nums[left]) {
+				right=mid;
+			}else if(nums[mid]>nums[right]) {
+				left=mid;
 			}
 		}
- 	return ret;
+		return Math.min(nums[left], nums[right]);
 	}
 }
--- src/com/demo/offer/Solution08.java
+++ src/com/demo/offer/Solution08.java
@@ -6,30 +6,23 @@ public class Solution08 {
 	 *  输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。 
 	 *  例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的所有元素都大于0，若数组大小为0，请返回0
 	 *  
-	 *  思路：利用二分法，找到中间的数，然后和最左边的值进行比较，若大于最左边的数，
-	 *  则最左边从mid开始，若小于最右边值，则最右边从mid开始。若左中右三值相等，
-	 *  则取mid前后值中较小的数。
+	 *  思路：通过比较第一个和最后一个数的大小，如果第一个数小，则没有旋转，直接返回这个数。
+	如果第一个数大，就要进一步搜索。我们定义left和right两个指针分别指向开头和结尾，还要找到中间那个数，
+	然后和left指的数比较，如果中间的数大，则继续二分查找右半段数组，反之查找左半段。终止条件是当左右两个指针相邻，
+	返回小的那个。
 	 */
 	public int minNumberInRotateArray(int[] nums) {
-		if(nums.length==0 || nums==null) return 0;
-		int left=0;
-		int right=nums.length-1;
-		int res=Integer.MAX_VALUE;
-		while(left<=right) {
-			int mid=(right-left)/2+left;
-			//无翻转
-			if(nums[left]<nums[right]) {
-				return Math.min(nums[left], res);
-			}else {
-				// num[mid] is on the left rotated part.
-				if(nums[mid]>=nums[left]) {
-					left=mid+1;
-				}else {// num[mid] is on the right rotated part.
-					res=nums[mid];
-					right=mid-1;
-				}
+		if(nums.length==1) return nums[0];
+		int left = 0, right = nums.length - 1;
+		if(nums[left]<nums[right]) return nums[0];
+		while(left+1 != right) {
+			int mid=left+(right-left)/2;
+			if(nums[mid]<nums[left]) {
+				right=mid;
+			}else if(nums[mid]>nums[right]) {
+				left=mid;
 			}
 		}
-		return res;
+		return Math.min(nums[left], nums[right]);
 	}
 }
